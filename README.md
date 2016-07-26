f380d9e3c2db3aa714f15b0109f6a4f0e35f664e
Jessica Dussault <jdussault4@gmail.com>
2016-07-26 14:32:58 -0500

    Adds the rest of the about subpages

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # open-oni_nebraska_theme
 This is the new nebraska child theme for open-oni
+
+## Including in the Open ONI App
+
+In `onisite/settings_local.py`, add the theme.  Don't remove `themes.default`.
+
+```
+INSTALLED_APPS = (
+    'django.contrib.humanize',
+    'django.contrib.staticfiles',
+
+    'themes.nebraska',
+    'themes.default',
+    'core',
+)
+
+```
+
+Also, in `onisite/urls.py`, add the Nebraska about URLs:
+
+```
+from django.conf.urls import url, include
+
+urlpatterns = [
+  url(r'^about/', include("themes.nebraska.urls")),
+  url('', include("core.urls")),
+]
+
+```
--- views.py
+++ views.py
@@ -1,8 +1,45 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+def about(request):
+    page_title = "About Nebraska Newspapers"
+    return render_to_response('about.html', 
+                            dictionary=locals(), 
+                            context_instance=RequestContext(request))
+
+def access(request):
+    page_title = "About Nebraska Newspapers"
+    return render_to_response('custom/access.html', 
+                            dictionary=locals(), 
+                            context_instance=RequestContext(request))
+def adding(request):
+    page_title = "About Nebraska Newspapers"
+    return render_to_response('custom/adding.html', 
+                            dictionary=locals(), 
+                            context_instance=RequestContext(request))
+
+def contact(request):
+    page_title = "About Nebraska Newspapers"
+    return render_to_response('custom/contact.html', 
+                            dictionary=locals(), 
+                            context_instance=RequestContext(request))
+
+def nnp(request):
+    page_title = "About Nebraska Newspapers"
+    return render_to_response('custom/nnp.html', 
+                            dictionary=locals(), 
+                            context_instance=RequestContext(request))
+
+def ndnp(request):
+    page_title = "About Nebraska Newspapers"
+    return render_to_response('custom/ndnp.html', 
+                            dictionary=locals(), 
+                            context_instance=RequestContext(request))
+
 def publishing(request):
     page_title = "Publishing History"
     return render_to_response('custom/publishing.html', 
                             dictionary=locals(), 
                             context_instance=RequestContext(request))
+
+
