87079e53f38bfdcd06744350d6db27f796466f98
Jessica Dussault <jdussault4@gmail.com>
2019-02-25 10:59:27 -0600

    updates and consolidates about pages

--- urls.py
+++ urls.py
@@ -2,13 +2,6 @@ from django.conf.urls import url, include
 from themes.nebraska import views
 
 urlpatterns = [
-  # about subpages
-  url('access', views.access, name="nebraska_about_access"),
-  url('adding', views.adding, name="nebraska_about_adding"),
-  url('contact', views.contact, name="nebraska_about_contact"),
-  url('ndnp', views.ndnp, name="nebraska_about_ndnp"),
-  url('nnp', views.nnp, name="nebraska_about_nnp"),
-
   # publishing history
   url('publishing', views.publishing, name="nebraska_publishing"),
 ]
--- views.py
+++ views.py
@@ -5,26 +5,6 @@ def about(request):
     page_title = "About Nebraska Newspapers"
     return render(request, 'about.html', locals())
 
-def access(request):
-    page_title = "About Nebraska Newspapers"
-    return render(request, 'custom/access.html', locals())
-
-def adding(request):
-    page_title = "About Nebraska Newspapers"
-    return render(request, 'custom/adding.html', locals())
-
-def contact(request):
-    page_title = "About Nebraska Newspapers"
-    return render(request, 'custom/contact.html', locals())
-
-def nnp(request):
-    page_title = "About Nebraska Newspapers"
-    return render(request, 'custom/nnp.html', locals())
-
-def ndnp(request):
-    page_title = "About Nebraska Newspapers"
-    return render(request, 'custom/ndnp.html', locals())
-
 def publishing(request):
     page_title = "Publishing History"
     return render(request, 'custom/publishing.html', locals())
