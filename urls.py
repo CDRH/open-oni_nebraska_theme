fe0be4c4fc3f6d5e1973b818296bdd6307360faa
Greg Tunink <techgique@gmail.com>
2022-02-02 20:00:55 -0600

    Update urls.py code
    django.conf.urls and url() function are deprecated
    
    https://docs.djangoproject.com/en/2.2/ref/urls/#url

--- urls.py
+++ urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls import url, include
+from django.urls import include, path, re_path
 from themes.nebraska import views
 
 urlpatterns = [
   # about subpages
-  url('access', views.access, name="nebraska_about_access"),
-  url('adding', views.adding, name="nebraska_about_adding"),
-  url('contact', views.contact, name="nebraska_about_contact"),
-  url('nnp', views.nnp, name="nebraska_about_nnp"),
+  path('access', views.access, name="nebraska_about_access"),
+  path('adding', views.adding, name="nebraska_about_adding"),
+  path('contact', views.contact, name="nebraska_about_contact"),
+  path('nnp', views.nnp, name="nebraska_about_nnp"),
 
   # publishing history
-  url('publishing', views.publishing, name="nebraska_publishing"),
+  path('publishing', views.publishing, name="nebraska_publishing"),
 ]
