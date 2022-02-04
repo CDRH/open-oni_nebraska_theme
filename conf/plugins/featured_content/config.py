a10267ec717004558385ee28286e17f2a3290d71
Greg Tunink <techgique@gmail.com>
2022-02-03 19:45:40 -0600

    Add featured_content "this day" and years config

---
+++ conf/plugins/featured_content/config.py
@@ -0,0 +1,32 @@
+# Set to True to use random pages instead of curated selections in PAGES
+RANDOM = False
+
+# Set to True to enable the 'This day in history' function
+THISDAY = True
+
+# Set to earliest year to search with THISDAY
+MINYEAR = 1854
+
+# Set to latest year to search  with THISDAY
+MAXYEAR = 2001
+
+# The number of results that will be displayed
+NUMBER = 4
+
+# Specific featured pages to display
+PAGES = (
+          {
+              'lccn': 'sn83045350',
+              'date': '1878-01-03',
+              'edition': 1,
+              'sequence': 1,
+              'caption': 'Put a caption for your newspaper here'
+          },
+          {
+              'lccn': 'sn83045350',
+              'date': '1878-01-03',
+              'edition': 1,
+              'sequence': 1,
+              'caption': 'This is the second one'
+          },
+        )
